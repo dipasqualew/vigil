99b5e9791b369110f97fce9225f7dccd7a9882a1
William Di Pasquale <wdp@dipasqualew.com>
2024-10-23 23:56:39 +0100

    Pipeline

--- packages/e2e-web/playwright.config.ts
+++ packages/e2e-web/playwright.config.ts
@@ -35,13 +35,10 @@ export default defineConfig({
     reporter: [
         ['html', { outputFolder: path.join(utils.CACHE_FOLDER, "html-report") }],
     ],
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL,
-
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        screenshot: process.env.CI ? "only-on-failure" : 'off',
+        trace: 'retain-on-failure',
     },
 
     /* Configure projects for major browsers */
