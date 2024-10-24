acae95a533d12adfb88e8b17a81497e75e509962
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2024-10-24 22:43:16 +0200

    chore: improve feed UI

--- packages/web/src/components/templates/BaseTemplate.vue
+++ packages/web/src/components/templates/BaseTemplate.vue
@@ -1,32 +1,39 @@
 <template>
-    <header>
-        <v-toolbar border density="compact">
-            <v-toolbar-title><router-link to="/">Vigil</router-link></v-toolbar-title>
-            <v-spacer></v-spacer>
-            <router-link to="/ingest">
-                <v-icon icon="mdi-plus-box" />
-            </router-link>
-
-        </v-toolbar>
-    </header>
-    <main>
-        <slot />
-    </main>
+  <v-app>
+    <v-app-bar :elevation="1">
+      <template v-slot:append>
+        <v-app-bar-nav-icon>
+          <router-link to="/ingest">
+            <v-icon icon="mdi-plus-box" />
+          </router-link>
+        </v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title><router-link to="/">Vigil</router-link></v-app-bar-title>
+    </v-app-bar>
+    <v-main class="layout">
+      <slot />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
 main {
-    max-width: 1600px;
-    width: 100%;
-    padding: 2em;
+  max-width: 1600px;
+  width: 100%;
+  padding: 2em;
 }
 
 a {
-    color: inherit;
-    text-decoration: none;
+  color: inherit;
+  text-decoration: none;
 }
 
 i {
-    padding: 1rem
+  padding: 1rem;
+}
+
+.layout {
+  padding-top: var(--v-layout-top);
+  padding-bottom: calc(var(--v-layout-bottom) + 16px);
 }
 </style>
--- packages/web/src/components/usercontent/MediaSummary.vue
+++ packages/web/src/components/usercontent/MediaSummary.vue
@@ -1,74 +1,95 @@
 <template>
-    <v-container class="content">
-        <v-row justify="space-between">
-            <v-col cols="9">
-                <strong>{{ relativeTime }}</strong>: {{ description }}
-            </v-col>
-            <v-col cols="3">
-                <v-dialog max-width="500">
-                    <template v-slot:activator="{ props: activatorProps }">
-                        <div class="details">
-                            <router-link :to="`/usercontent/${media.key}`">
-                                <v-icon size="small" icon="mdi-link" />
-                            </router-link>
-                            <v-icon size="small" v-bind="activatorProps" icon="mdi-information" />
-                            <v-icon size="small" color="red" @click="deleteContent" icon="mdi-delete" />
-                        </div>
-                    </template>
-                    <template v-slot:default="{ isActive }">
-                        <v-card :title="media.filename">
-                            <v-card-text>
-                                <div><strong>Key</strong>: {{ media.key }}</div>
-                                <div><strong>Filename</strong>: {{ media.filename }}</div>
-                                <div><strong>Created</strong>: {{ new Date(media.created).toLocaleString() }}</div>
-                                <div><strong>Summary</strong>: {{ media.description }}</div>
-                                <div v-for="result in results" :key="result.key">
-                                    <div><strong>Key</strong>: {{ result.key }}</div>
-                                    <div><strong>Type</strong>: {{ result.actionType }}</div>
-                                    <div>
-                                        <pre>{{ result.value }}</pre>
-                                    </div>
-                                </div>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn text="Close Details" @click="isActive.value = false"></v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </template>
-                </v-dialog>
-            </v-col>
-        </v-row>
-        <S3Renderer class="renderer" :signed-url="url" :content-type="media.contentType" />
-    </v-container>
+  <v-card>
+    <v-toolbar color="transparent">
+      <v-toolbar-title class="text-h6">
+        <v-toolbar-title-text>{{ relativeTime }}</v-toolbar-title-text>
+      </v-toolbar-title>
+      <template v-slot:append>
+        <v-btn
+          color="primary"
+          icon="mdi-link"
+          :to="`/usercontent/${media.key}`"
+        ></v-btn>
+
+        <v-dialog max-width="500">
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-btn
+              color="secondary"
+              icon="mdi-information"
+              v-bind="activatorProps"
+            >
+            </v-btn>
+          </template>
+          <template v-slot:default="{ isActive }">
+            <v-card :title="media.filename">
+              <v-card-text>
+                <div><strong>Key</strong>: {{ media.key }}</div>
+                <div><strong>Filename</strong>: {{ media.filename }}</div>
+                <div>
+                  <strong>Created</strong>:
+                  {{ new Date(media.created).toLocaleString() }}
+                </div>
+                <div><strong>Summary</strong>: {{ media.description }}</div>
+                <div v-for="result in results" :key="result.key">
+                  <div><strong>Key</strong>: {{ result.key }}</div>
+                  <div><strong>Type</strong>: {{ result.actionType }}</div>
+                  <div>
+                    <pre>{{ result.value }}</pre>
+                  </div>
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  text="Close Details"
+                  @click="isActive.value = false"
+                ></v-btn>
+              </v-card-actions>
+            </v-card> </template
+        ></v-dialog>
+        <v-btn color="error" icon="mdi-delete" @click="deleteContent"></v-btn>
+      </template>
+    </v-toolbar>
+    <v-card-subtitle>{{ description }}</v-card-subtitle>
+    <v-card-text>
+      <S3Renderer
+        class="renderer"
+        :signed-url="url"
+        :content-type="media.contentType"
+    /></v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
-import { useRouter } from 'vue-router';
+import { computed } from "vue";
+import { useRouter } from "vue-router";
 
-import S3Renderer from '~/web/components/usercontent/S3Renderer.vue';
-import { ActionResult, Entities, Media, useMediaService } from '~/web/services/media';
-import { IngestSources } from '~/web/types';
-import { getRelativeTime } from '~/web/utils';
+import { IngestSources } from "~/web/types";
+import S3Renderer from "~/web/components/usercontent/S3Renderer.vue";
+import {
+  ActionResult,
+  Entities,
+  Media,
+  useMediaService,
+} from "~/web/services/media";
+import { getRelativeTime } from "~/web/utils";
 
-
-const { media } = defineProps<{ media: Media, results: ActionResult[] }>();
+const { media } = defineProps<{ media: Media; results: ActionResult[] }>();
 const router = useRouter();
 
 const description = computed(() => {
-    switch (media.source) {
-        case IngestSources.TXT_NOTE:
-            return 'You wrote a note';
-        case IngestSources.FILE_UPLOAD:
-            return 'You uploaded a file';
-        case IngestSources.PHOTO:
-            return 'You took a photo';
-        case IngestSources.AUDIO_RECORDING:
-            return 'You recorded an audio note';
-        default:
-            return 'Unknown source';
-    }
+  switch (media.source) {
+    case IngestSources.TXT_NOTE:
+      return "You wrote a note";
+    case IngestSources.FILE_UPLOAD:
+      return "You uploaded a file";
+    case IngestSources.PHOTO:
+      return "You took a photo";
+    case IngestSources.AUDIO_RECORDING:
+      return "You recorded an audio note";
+    default:
+      return "Unknown source";
+  }
 });
 
 const url = computed(() => URL.createObjectURL(media.blob));
@@ -76,28 +97,28 @@ const url = computed(() => URL.createObjectURL(media.blob));
 const relativeTime = computed(() => getRelativeTime(new Date(media.created)));
 
 const deleteContent = async () => {
-    const mediaService = useMediaService();
-    await mediaService.crud.delete(Entities.MEDIA, media.key);
-    await router.push("/");
+  const mediaService = useMediaService();
+  await mediaService.crud.delete(Entities.MEDIA, media.key);
+  await router.push("/");
 };
 </script>
 
 <style scoped>
 i {
-    padding: 0.75rem;
+  padding: 0.75rem;
 }
 
 .content {
-    border: 1px solid black;
-    margin: 1rem;
-    padding: 1rem;
+  border: 1px solid black;
+  margin: 1rem;
+  padding: 1rem;
 }
 
 .details {
-    text-align: right;
+  text-align: right;
 }
 
 .renderer {
-    margin-top: 1rem;
+  margin-top: 1rem;
 }
 </style>
