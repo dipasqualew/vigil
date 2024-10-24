6cac3bd194ca1414289719415d06b45180ae394f
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2024-10-24 23:02:26 +0200

    chore: improve feed UI (#11)

--- packages/web/src/views/UserContentView.vue
+++ packages/web/src/views/UserContentView.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, ref, watch } from "vue";
 
-import MediaSummary from '~/web/components/usercontent/MediaSummary.vue';
-import ResultContainer from '~/web/components/usercontent/ResultContainer.vue';
-import { ActionResult, Entities, Media, useMediaService } from '~/web/services/media';
+import MediaSummary from "~/web/components/usercontent/MediaSummary.vue";
+import ResultContainer from "~/web/components/usercontent/ResultContainer.vue";
+import {
+    ActionResult,
+    Entities,
+    Media,
+    useMediaService,
+} from "~/web/services/media";
 
 
 const { contentUuid } = defineProps<{ contentUuid?: string }>();
 
 const displayRecords = ref(false);
-const entities = ref<{ media: Media, results: ActionResult[] }[]>([]);
+const entities = ref<{ media: Media; results: ActionResult[] }[]>([]);
 
 const loadContent = async (mediaKey?: string) => {
     const mediaService = useMediaService();
@@ -24,7 +29,9 @@ const loadContent = async (mediaKey?: string) => {
         });
 
     for (const item of items) {
-        const result = await mediaService.crud.list(Entities.ACTION_RESULT, { mediaKey: item.key });
+        const result = await mediaService.crud.list(Entities.ACTION_RESULT, {
+            mediaKey: item.key,
+        });
 
         entities.value.push({ media: item, results: result });
     }
@@ -34,56 +41,55 @@ onMounted(async () => {
     await loadContent(contentUuid);
 });
 
-
-watch(() => contentUuid, async (newUuid) => {
-    entities.value = [];
-    await loadContent(newUuid);
-});
+watch(
+    () => contentUuid,
+    async (newUuid) => {
+        entities.value = [];
+        await loadContent(newUuid);
+    },
+);
 </script>
 
 <template>
-    <v-switch v-model="displayRecords" :label="displayRecords ? 'Showing Records' : 'Showing Sources'" />
-    <div class="add">
-        <router-link to="/ingest/text"><v-icon icon="mdi-text" /></router-link>
-        <router-link to="/ingest/photo"><v-icon icon="mdi-camera" /></router-link>
-        <router-link to="/ingest/audio"><v-icon icon="mdi-microphone" /></router-link>
-        <router-link to="/ingest/file"><v-icon icon="mdi-file" /></router-link>
-    </div>
-    <div v-for="entity in entities" :key="entity.media.key">
-        <div v-if="displayRecords">
-            <div v-for="result in entity.results" :key="result.key">
-                <ResultContainer
-                    :result="result"
-                    :media="entity.media" />
-            </div>
+  <v-switch
+    v-model="displayRecords"
+    :label="displayRecords ? 'Showing Records' : 'Showing Sources'"
+  />
+  <v-row dense>
+    <v-col v-for="entity in entities" :key="entity.media.key">
+      <div v-if="displayRecords">
+        <div v-for="result in entity.results" :key="result.key">
+          <ResultContainer :result="result" :media="entity.media" />
         </div>
-        <div v-else>
-            <MediaSummary :media="entity.media" :results="entity.results" />
-            <div class="add">
-                <router-link to="/ingest/text"><v-icon icon="mdi-text" /></router-link>
-                <router-link to="/ingest/photo"><v-icon icon="mdi-camera" /></router-link>
-                <router-link to="/ingest/audio"><v-icon icon="mdi-microphone" /></router-link>
-                <router-link to="/ingest/file"><v-icon icon="mdi-file" /></router-link>
-            </div>
-        </div>
-    </div>
+      </div>
+      <div v-else>
+        <MediaSummary :media="entity.media" :results="entity.results" />
+      </div>
+    </v-col>
+  </v-row>
+  <v-bottom-navigation>
+    <v-btn value="text" to="/ingest/text">
+      <v-icon icon="mdi-text" />
+      <span>Text</span>
+    </v-btn>
+    <v-btn value="photo" to="/ingest/photo">
+      <v-icon icon="mdi-camera" />
+      <span>Photo</span>
+    </v-btn>
+    <v-btn value="photo" to="/ingest/audio">
+      <v-icon icon="mdi-microphone" />
+      <span>Audio</span>
+    </v-btn>
+    <v-btn value="file" to="/ingest/file">
+      <v-icon icon="mdi-file" />
+      <span>File</span>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <style scoped>
 a {
-    text-decoration: none;
-    color: inherit;
-}
-.add {
-    text-align: center;
-    color: transparent;
-    transition: color 0.6s;
-    cursor: pointer;
-}
-.add i {
-    margin: 0 0.2rem;
-}
-.add:hover {
-    color: black;
+  text-decoration: none;
+  color: inherit;
 }
 </style>
